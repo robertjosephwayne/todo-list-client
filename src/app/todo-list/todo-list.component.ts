cb1deb924fe337cbf91cbd5b2c8347a0ee7dd999
Robert <robertjwayne@gmail.com>
2020-12-22 17:20:16 -0800

    Create TodoListComponent

---
+++ src/app/todo-list/todo-list.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { Todo } from '../models/todo.model';
+
+import * as TodoListActions from '../store/todo-list/todo-list.actions';
+import * as fromTodoList from '../store/todo-list/todo-list.selectors';
+
+@Component({
+  selector: 'app-todo-list',
+  templateUrl: './todo-list.component.html',
+  styleUrls: ['./todo-list.component.css']
+})
+export class TodoListComponent implements OnInit {
+  todos$: Observable<Todo[]>;
+
+  constructor(private store: Store) { }
+
+  ngOnInit(): void {
+    this.todos$ = this.store.select(fromTodoList.selectAllTodos);
+    this.fetchAll();
+  }
+
+  fetchAll(): void {
+    this.store.dispatch(TodoListActions.fetchTodoList());
+  }
+}
