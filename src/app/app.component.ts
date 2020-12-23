8e247437b3751b34852aafcf1992123a6996d589
Robert <robertjwayne@gmail.com>
2020-12-22 17:11:34 -0800

    Remove title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'todo-list-client';
-}
+export class AppComponent {}
