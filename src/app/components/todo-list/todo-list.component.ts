788dd0a9367e5315c1cb1ff600affff870baff5e
Robert <robertjwayne@gmail.com>
2020-12-22 23:13:57 -0800

    Create expandable Material table for todo list

--- src/app/components/todo-list/todo-list.component.ts
+++ src/app/components/todo-list/todo-list.component.ts
@@ -1,6 +1,9 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { Observable } from 'rxjs';
+
+import { Subscription } from 'rxjs';
+
 import { Todo } from '../../models/todo.model';
 
 import * as TodoListActions from '../../store/todo-list/todo-list.actions';
@@ -9,19 +12,35 @@ import * as fromTodoList from '../../store/todo-list/todo-list.selectors';
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
-  styleUrls: ['./todo-list.component.css']
+  styleUrls: ['./todo-list.component.css'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({ height: '0px', minHeight: '0' })),
+      state('expanded', style({ height: '*' })),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ],
 })
 export class TodoListComponent implements OnInit {
-  todos$: Observable<Todo[]>;
+  todos: Todo[];
+  todosSub: Subscription;
+  columnsToDisplay = ['title', 'isComplete'];
+  expandedTodo: Todo | null;
 
   constructor(private store: Store) { }
 
   ngOnInit(): void {
-    this.todos$ = this.store.select(fromTodoList.selectAllTodos);
+    this.todosSub = this.store.select(fromTodoList.selectAllTodos).subscribe(todos => {
+      this.todos = todos;
+    });
     this.fetchAll();
   }
 
   fetchAll(): void {
     this.store.dispatch(TodoListActions.fetchTodoList());
+  }
+
+  ngOnDestroy(): void {
+    this.todosSub.unsubscribe();
   }
 }
