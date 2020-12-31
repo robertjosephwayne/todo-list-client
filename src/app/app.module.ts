d2a146f8a5e26701def10c5d876873ed3ed08859
Robert <robertjwayne@gmail.com>
2020-12-30 18:48:03 -0800

    Import HTTP_INTERCEPTORS

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
