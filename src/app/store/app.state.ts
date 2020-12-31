d3bbf7af6392993ae97bede32a237dee6fea0de1
Robert <robertjwayne@gmail.com>
2020-12-30 18:55:53 -0800

    Add auth state

--- src/app/store/app.state.ts
+++ src/app/store/app.state.ts
@@ -1,5 +1,8 @@
+import { AuthState } from './auth/auth.state';
+import { RouterState } from './router/router.state';
 import { TodoListState } from './todo-list/todo-list.state';
 
 export interface AppState {
+  auth: AuthState;
   todoList: TodoListState;
 }
