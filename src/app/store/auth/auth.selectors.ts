6b64bd9997a07ac35f6aa90faa0e6ca13c9d3ef6
Robert <robertjwayne@gmail.com>
2020-12-30 18:57:57 -0800

    Create selectors
    - Add selectAuth selector
    - Add selectJWTToken selector
    - Add selectIsAuth selector

---
+++ src/app/store/auth/auth.selectors.ts
@@ -0,0 +1,15 @@
+import { createSelector } from '@ngrx/store';
+
+import { AppState } from '../app.state';
+
+export const selectAuth = (state: AppState) => state.auth;
+
+export const selectJWTToken = createSelector(
+  selectAuth,
+  (auth) => auth.jwtToken
+);
+
+export const selectIsAuth = createSelector(
+  selectJWTToken,
+  (jwtToken) => !!jwtToken
+);
