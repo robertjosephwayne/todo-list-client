97e6b5465535e0bfe390b985293f06f6a00ed08f
Robert <robertjwayne@gmail.com>
2020-12-30 20:20:42 -0800

    Add isAuth property to hold user auth state

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,18 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { AuthService } from 'src/app/services/auth.service';
 
+import * as fromAuth from '../../store/auth/auth.selectors';
 import * as AuthActions from '../../store/auth/auth.actions';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
   label = "To-Do List";
+  isAuth$: Observable<boolean>;
 
   constructor(private store: Store) { }
+
+  ngOnInit(): void {
+    this.setAuthState();
+  }
+
+  setAuthState(): void {
+    this.isAuth$ = this.store.select(fromAuth.selectIsAuth);
+  }
 
   onLogout(): void {
     this.store.dispatch(AuthActions.logout());
