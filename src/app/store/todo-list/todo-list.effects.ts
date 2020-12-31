e0f97a72a37a1c08a55d26aae3f2b50d72b922de
Robert <robertjwayne@gmail.com>
2020-12-30 20:26:05 -0800

    Add createTodoItemSuccess$ effect to redirect user to '/' path

--- src/app/store/todo-list/todo-list.effects.ts
+++ src/app/store/todo-list/todo-list.effects.ts
@@ -4,13 +4,14 @@ import { HttpClient } from '@angular/common/http';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { Store } from '@ngrx/store';
 
-import { map, withLatestFrom } from 'rxjs/operators';
+import { map, tap, withLatestFrom } from 'rxjs/operators';
 
 import { AppState } from '../app.state';
 
 import * as TodoListActions from './todo-list.actions';
 
 import { TodoListService } from 'src/app/services/todo-list.service';
+import { Router } from '@angular/router';
 
 
 
@@ -35,6 +36,13 @@ export class TodoListEffects {
         action.title,
         action.description
       );
+    })
+  ), { dispatch: false });
+
+  createTodoItemSuccess$ = createEffect(() => this.actions$.pipe(
+    ofType(TodoListActions.createTodoItemSuccess),
+    tap(() => {
+      this.router.navigate(['/']);
     })
   ), { dispatch: false });
 
@@ -74,7 +82,7 @@ export class TodoListEffects {
   constructor(
     private actions$: Actions,
     private store: Store<AppState>,
-
+    private router: Router,
     private todoListService: TodoListService
   ) { }
 }
