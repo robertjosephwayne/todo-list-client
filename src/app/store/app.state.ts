06d6403d3f4a0520855f88c0fee20f749d5193ca
Robert <robertjwayne@gmail.com>
2020-12-31 00:43:50 -0800

    Add router state

--- src/app/store/app.state.ts
+++ src/app/store/app.state.ts
@@ -1,8 +1,10 @@
+import { RouterReducerState } from '@ngrx/router-store';
 import { AuthState } from './auth/auth.state';
 import { RouterState } from './router/router.state';
 import { TodoListState } from './todo-list/todo-list.state';
 
 export interface AppState {
   auth: AuthState;
+  router: RouterReducerState<any>;
   todoList: TodoListState;
 }
