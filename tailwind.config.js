794dff053c453c9442ae7d3dac755531c4e53563
Robert <robertjwayne@gmail.com>
2021-01-05 19:18:30 -0800

    Create custom color theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,28 @@
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   purge: ["./src/**/*.{html,ts}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+
+      black: "#000",
+      white: "#fff",
+
+      emerald: colors.emerald,
+      gray: colors.blueGray,
+      blue: colors.lightBlue,
+      purple: colors.violet,
+      red: colors.red,
+      yellow: colors.yellow,
+    },
   },
   variants: {
     extend: {
       borderWidth: ["hover"],
+      visibility: ["group-hover"],
     },
   },
   plugins: [],
