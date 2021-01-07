6dc779a6ab4c616f53a2df8c2d327b8d544ada73
Robert <robertjwayne@gmail.com>
2021-01-06 19:27:58 -0800

    Add @Injectable()

--- src/app/components/sidenav/sidenav.store.ts
+++ src/app/components/sidenav/sidenav.store.ts
@@ -14,6 +14,7 @@ const initialState: SidenavState = {
   drawerMode: 'side'
 };
 
+@Injectable()
 export class SidenavStore extends ComponentStore<SidenavState> {
   constructor() {
     super(initialState);
