b2b4adce62f30d7d72ed937c300c4b0109b654c5
Robert <robertjwayne@gmail.com>
2021-01-07 00:34:28 -0800

    Remove AuthService

--- src/app/services/auth-interceptor.ts
+++ src/app/services/auth-interceptor.ts
@@ -1,11 +1,10 @@
 import { HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { AuthService } from './auth.service';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
 
-  constructor(private authService: AuthService) { }
+  constructor() { }
 
   intercept(req: HttpRequest<any>, next: HttpHandler) {
     const authToken = localStorage.getItem('jwtToken');
