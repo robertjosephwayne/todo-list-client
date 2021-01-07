e1dc482894d0642281736df34e8870aac895afda
Robert <robertjwayne@gmail.com>
2021-01-06 19:23:57 -0800

    Remove sidenav logic from app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,10 @@
-import { Component, HostListener, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { SidenavStore } from './components/sidenav/sidenav.store';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [SidenavStore]
 })
-export class AppComponent implements OnInit {
-  drawerMode: string;
-
-  constructor() { }
-
-  ngOnInit(): void {
-    this.setDrawerMode();
-  }
-
-  @HostListener('window:resize', ['$event'])
-  setDrawerMode(): void {
-    if (window.innerWidth < 700) {
-      this.drawerMode = 'over'
-    } else {
-      this.drawerMode = 'side';
-    }
-  }
-
-}
+export class AppComponent { }
