e363d5ba2793efc7d1c1ec454a33729a8f47619b
Robert <robertjwayne@gmail.com>
2021-01-06 19:19:15 -0800

    Create SidenavDrawerComponent

---
+++ src/app/components/sidenav/sidenav-drawer/sidenav-drawer.component.ts
@@ -0,0 +1,13 @@
+import { Component, HostListener, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { MatDrawer } from '@angular/material/sidenav';
+
+import { SidenavStore } from '../sidenav.store';
+
+@Component({
+  selector: 'app-sidenav-drawer',
+  templateUrl: './sidenav-drawer.component.html',
+  styleUrls: ['./sidenav-drawer.component.css']
+})
+export class SidenavDrawerComponent {
+
+}
