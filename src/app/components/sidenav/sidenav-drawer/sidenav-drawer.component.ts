e58b016fe4422797ceca92a76956cd2024af6f06
Robert <robertjwayne@gmail.com>
2021-01-08 18:46:45 -0800

    Create initial version of sidenav drawer

--- src/app/components/sidenav/sidenav-drawer/sidenav-drawer.component.ts
+++ src/app/components/sidenav/sidenav-drawer/sidenav-drawer.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SidenavStore } from '../sidenav.store';
+import { TodoListStore } from '../todo-list/todo-list.store';
 
 @Component({
   selector: 'app-sidenav-drawer',
   templateUrl: './sidenav-drawer.component.html',
   styleUrls: ['./sidenav-drawer.component.css']
 })
-export class SidenavDrawerComponent {
+export class SidenavDrawerComponent implements OnInit {
+  readonly projectListOpen$ = this.sidenavStore.projectListOpen$;
+  readonly projectNames$ = this.todoListStore.projectNames$;
+  readonly selectedProject$ = this.todoListStore.selectedProject$;
 
+  constructor(
+    private readonly sidenavStore: SidenavStore,
+    private readonly todoListStore: TodoListStore
+  ) { }
+
+  ngOnInit(): void { }
+
+  selectProject(project: string): void {
+    this.todoListStore.setSelectedProject(project);
+  }
+
+  toggleProjectList(): void {
+    this.sidenavStore.toggleProjectList();
+  }
 }
