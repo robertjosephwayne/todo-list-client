1d2b53240b4c43ad9fdee5f645a898a5b7ea6af1
Robert <robertjwayne@gmail.com>
2021-01-09 02:41:42 -0800

    Add createProject() and getProjects()

--- src/app/services/todo-list.service.ts
+++ src/app/services/todo-list.service.ts
@@ -1,7 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
+import { NewProject } from '../models/new-project.model';
 import { NewTodo } from '../models/new-todo.model';
+import { Project } from '../models/project.model';
 import { Todo, TodoId } from '../models/todo.model';
 
 @Injectable({ providedIn: 'root' })
@@ -9,17 +11,27 @@ export class TodoListService {
 
   constructor(private http: HttpClient) { }
 
+  createTodo(newTodo: NewTodo) {
+    return this.http.post<Todo>('http://localhost:3000/todos', newTodo);
+  }
+
+  createProject(newProject: NewProject) {
+    debugger;
+    return this.http.post<Project>('http://localhost:3000/projects', newProject);
+  }
+
   getTodos() {
     return this.http.get<Todo[]>('http://localhost:3000/todos');
+  }
+
+  getProjects() {
+    return this.http.get<Project[]>('http://localhost:3000/projects');
   }
 
   deleteTodo(todoId: TodoId) {
     return this.http.delete(`http://localhost:3000/todos/${todoId}`);
   }
 
-  createTodo(newTodo: NewTodo) {
-    return this.http.post<Todo>('http://localhost:3000/todos', newTodo);
-  }
 
   editTodo(updatedTodo: Todo) {
     return this.http.patch(`http://localhost:3000/todos/${updatedTodo.id}`, {
