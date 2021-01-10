316eceed54034ab56745fd589fd5262da3e67f14
Robert <robertjwayne@gmail.com>
2021-01-09 18:33:50 -0800

    Code formatting

--- src/app/services/todo-list.service.ts
+++ src/app/services/todo-list.service.ts
@@ -11,13 +11,23 @@ export class TodoListService {
 
   constructor(private http: HttpClient) { }
 
-  // TODO: Update request using new routes (access todo through project)
   createTodo(newTodo: NewTodo) {
     return this.http.post<Todo>('http://localhost:3000/todos', newTodo);
   }
 
   createProject(newProject: NewProject) {
     return this.http.post<Project>('http://localhost:3000/projects', newProject);
+  }
+
+  deleteTodo(todoId: TodoId) {
+    return this.http.delete(`http://localhost:3000/todos/${todoId}`);
+  }
+
+  editTodo(updatedTodo: Todo) {
+    return this.http.patch(`http://localhost:3000/todos/${updatedTodo.id}`, {
+      title: updatedTodo.title,
+      isComplete: updatedTodo.isComplete
+    });
   }
 
   getTodos() {
@@ -28,18 +38,6 @@ export class TodoListService {
     return this.http.get<Project[]>('http://localhost:3000/projects');
   }
 
-  // TODO: Update request using new routes (access todo through project)
-  deleteTodo(todoId: TodoId) {
-    // return this.http.delete(`http://localhost:3000/todos/${todoId}`);
-  }
-
-  // TODO: Update request using new routes (access todo through project)
-  editTodo(updatedTodo: Todo) {
-    return this.http.patch(`http://localhost:3000/todos/${updatedTodo.id}`, {
-      title: updatedTodo.title,
-      isComplete: updatedTodo.isComplete
-    });
-  }
 }
 
 
