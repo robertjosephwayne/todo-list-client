8cddbff13fe4ba417bbe0636c7eeaf77751ea967
Robert <robertjwayne@gmail.com>
2021-01-09 18:00:35 -0800

    Code formatting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,41 +7,41 @@ import { BrowserModule } from '@angular/platform-browser';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { MaterialModule } from './material.module';
 
 import { AppComponent } from './app.component';
 import { appReducer } from './store/app.reducer';
 import { AppRoutingModule } from './app-routing.module';
+import { AuthEffects } from './store/auth/auth.effects';
+import { AuthInterceptor } from './services/auth-interceptor';
+import { CreateTodoDialogComponent } from './components/sidenav/todo-list/create-todo-dialog/create-todo-dialog.component';
+import { EditTodoDialogComponent } from './components/sidenav/todo-list/edit-todo-dialog/edit-todo-dialog.component';
 import { environment } from '../environments/environment';
 import { HeaderComponent } from './components/header/header.component';
 import { LoginComponent } from './components/sidenav/auth/login/login.component';
+import { MaterialModule } from './material.module';
+import { NewProjectDialogComponent } from './components/sidenav/sidenav-drawer/new-project-dialog/new-project-dialog.component';
+import { SidenavComponent } from './components/sidenav/sidenav.component';
+import { SidenavDrawerComponent } from './components/sidenav/sidenav-drawer/sidenav-drawer.component';
+import { SignupComponent } from './components/sidenav/auth/signup/signup.component';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
 import { TodoItemComponent } from './components/sidenav/todo-list/todo-item/todo-item.component';
 import { TodoListComponent } from './components/sidenav/todo-list/todo-list.component';
 import { TodoListEditorComponent } from './components/sidenav/todo-list/todo-list-editor/todo-list-editor.component';
-import { StoreRouterConnectingModule } from '@ngrx/router-store';
-import { AuthEffects } from './store/auth/auth.effects';
-import { SignupComponent } from './components/sidenav/auth/signup/signup.component';
-import { AuthInterceptor } from './services/auth-interceptor';
-import { SidenavComponent } from './components/sidenav/sidenav.component';
-import { SidenavDrawerComponent } from './components/sidenav/sidenav-drawer/sidenav-drawer.component';
-import { NewProjectDialogComponent } from './components/sidenav/sidenav-drawer/new-project-dialog/new-project-dialog.component';
-import { CreateTodoDialogComponent } from './create-todo-dialog/create-todo-dialog.component';
-import { EditTodoDialogComponent } from './edit-todo-dialog/edit-todo-dialog.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    LoginComponent,
-    SignupComponent,
-    TodoListComponent,
-    TodoItemComponent,
-    TodoListEditorComponent,
-    SidenavComponent,
-    SidenavDrawerComponent,
-    NewProjectDialogComponent,
     CreateTodoDialogComponent,
     EditTodoDialogComponent,
+    HeaderComponent,
+    LoginComponent,
+    NewProjectDialogComponent,
+    SidenavComponent,
+    SidenavDrawerComponent,
+    SignupComponent,
+    TodoItemComponent,
+    TodoListComponent,
+    TodoListEditorComponent,
   ],
   imports: [
     BrowserModule,
